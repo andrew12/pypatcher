0afab83c10062e62e34fc8017dc8a4b0b77cd630
Andrew Herbig <notandrewherbig@gmail.com>
2017-07-24 22:27:23 -0400

    Make it work

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pypatcher
 PyQt DLL patcher
+
+Most of this code was blatantly stolen from https://github.com/mon/BemaniPatcher but translated into Python
--- patcher.py
+++ patcher.py
@@ -1,26 +1,60 @@
 from __future__ import print_function
 
 from functools import partial
+import os
 import sys
 import yaml
+import mmap
 
-from qtpy.QtWidgets import *
+from PyQt5.QtWidgets import *
 
 def read(fp, offset, size):
     fp.seek(offset)
     data = fp.read(size)
     assert len(data) == size, "read %d bytes, expected %d" % (len(data), size)
 
-def write(fp, offset, data):
-    fp.seek(offset)
-    fp.write(data)
-
 class PatchCheckBox(QCheckBox):
     def __init__(self, group, mod):
-        QCheckBox.__init__(self, mod['name'])
+        self.name = mod['name']
+        QCheckBox.__init__(self, self.name)
         self.group = group
         self.patches = mod['patches']
+        if self.check():
+            self.setChecked(True)
         self.clicked.connect(self.doPatch)
+
+    def check(self):
+        status = None
+        for patch in self.patches:
+            offset = patch['offset']
+            on = bytes(patch[True])
+            off = bytes(patch[False])
+            assert len(on) == len(off)
+            if self.group.mmap[offset:offset+len(off)] == off:
+                if status is None:
+                    status = False
+                elif status != False:
+                    print('on/off mismatch')
+                    return
+            elif self.group.mmap[offset:offset+len(on)] == on:
+                if status is None:
+                    status = True
+                elif status != True:
+                    print('on/off mismatch')
+                    return
+            else:
+                print('neither on nor off')
+                return
+        return status
+
+    def validate(self):
+        status = self.check()
+        if status == True:
+            print(self.name, 'is enabled')
+        elif status == False:
+            print(self.name, 'is disabled')
+        else:
+            return '%r not found' % self.name
 
     def doPatch(self, checked):
         for patch in self.patches:
@@ -30,52 +64,85 @@ class PatchRadio(QRadioButton):
     def __init__(self, union, patch):
         QRadioButton.__init__(self, patch['name'])
         self.union = union
-        self.patch = patch
+        self.patch = bytes(patch['patch'])
+        if self.check():
+            self.setChecked(True)
         self.clicked.connect(self.doPatch)
+
+    def check(self):
+        return self.union.group.mmap[self.union.offset:self.union.offset+len(self.patch)] == self.patch
 
     def doPatch(self, state):
         self.union.write(self.patch)
 
 class PatchUnion(QGroupBox):
     def __init__(self, group, mod):
-        QGroupBox.__init__(self, mod['name'])
-        self.mod = mod
+        self.group = group
+        self.name = mod['name']
+        self.offset = mod['offset']
+        self.patches = mod['patches']
+        QGroupBox.__init__(self, self.name)
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
-        for patch in mod['patches']:
+        for patch in self.patches:
             radio = PatchRadio(self, patch)
             self.layout.addWidget(radio)
 
+    def validate(self):
+        for p in self.patches:
+            patch = p['patch']
+            if self.group.mmap[self.offset:self.offset+len(patch)] == bytes(patch):
+                print(self.name, 'has', p['name'])
+                return
+        return '%r not found' % self.name
+
     def write(self, patch):
-        self.group.write(self.mod['offset'], patch)
+        self.group.write(self.offset, patch)
 
 class PatchGroup(QGroupBox):
     def __init__(self, filename, mods):
-        # XXX - read in the file data
         filename += '.dll'
         QGroupBox.__init__(self, filename)
+        self.fileno = os.open(filename, os.O_RDWR | os.O_BINARY)
+        self.mmap = mmap.mmap(self.fileno, 0)
         self.filename = filename
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
+        self.mods = []
         for mod in mods:
             if 'type' in mod and mod['type'] == 'union':
-                self.layout.addWidget(PatchUnion(self, mod))
+                o = PatchUnion(self, mod)
             else:
-                self.layout.addWidget(PatchCheckBox(self, mod))
+                o = PatchCheckBox(self, mod)
+            self.mods.append(o)
+            self.layout.addWidget(o)
+        if self.validate():
+            print('DLL loaded successfully!')
+
+    def validate(self):
+        errors = []
+        for mod in self.mods:
+            error = mod.validate()
+            if error:
+                errors.append(error)
+        if errors:
+            QMessageBox.critical(self, 'Error', '\n'.join(errors))
+        return not errors
 
     def write(self, offset, patch):
-        # XXX - update the stored file data
+        self.mmap[offset:offset+len(patch)] = bytes(patch)
         print(self.filename, offset, patch)
-        # This could instead seek to the offset and write the data
-        # while the patcher is running
+        self.mmap.flush()
 
     def __del__(self):
-        # XXX - write out data
-        pass
+        self.mmap.flush()
+        self.mmap.close()
+        os.close(self.fileno)
 
 class Patcher(QWidget):
     def __init__(self, files):
         QWidget.__init__(self)
+        self.setWindowTitle('DLL Patcher')
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
         for filename, mods in files.items():
